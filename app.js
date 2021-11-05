2562a6003e494287d9066e90bfee3e71dc5d9bf2
Taleb Hammoudé <taleb.hammoude@outlook.com>
2021-11-05 16:22:13 +0100

    Första delen klar

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const div = document.createElement("div");
+const div2 = document.createElement("div");
+
+const span1 = document.createElement("span");
+const span2 = document.createElement("span");
+const span3 = document.createElement("span");
 
 document.body.appendChild(div);
 
-let titlesArray = ["h5", "h4", "h3", "h2", "h1"];
+let titlesArray = ["h6", "h4", "h3", "h2", "h1"];
+let raderArray = ["Rad 1", "Rad 2", "Rad 3", "Rad 4", "Rad 5"];
 
 
-
-for (let i=0; i<raderArray.length; i++) {
+for (let i=0; i<5; i++) {
 
     let createTitles = document.createElement(titlesArray[i]);
     createTitles.innerHTML = raderArray[i];
+    createTitles.setAttribute("style", "padding: 5px; border: 5px solid gray; text-align: center;");
     div.appendChild(createTitles);
     
-
 }
+
+
+document.body.appendChild(div2);
+
+div2.appendChild(span1);
+div2.appendChild(span2);
+div2.appendChild(span3);
+
+
+// let titlesArray = ["h6", "h4", "h3", "h2", "h1"];
+// let raderArray = ["Rad 1", "Rad 2", "Rad 3", "Rad 4", "Rad 5"];
+// let titlesArray = ["h6", "h4", "h3", "h2", "h1"];
+
